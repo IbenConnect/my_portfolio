4fda8a34bf6be84fd57390e9a8deb77b88891434
Imoh Ben <ibenconnect@gmail.com>
2026-08-18 20:32:18 +0100

    chore: initialize portfolio project

---
+++ config/site.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  name: "My Website",
+  description: "A modern portfolio web application built with Next.js.",
+  url: "http://localhost:3000",
+  author: "Iben Connect"
+}
